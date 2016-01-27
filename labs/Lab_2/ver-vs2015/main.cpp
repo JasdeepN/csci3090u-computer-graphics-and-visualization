7d1d36cb29aa7c42698dc996fdc3a0c8299c99c3
Jasdeep Nijjar <100493157@oncampus.local>
2016-01-27 13:45:16 -0500

    displays object

--- labs/Lab_2/ver-vs2015/main.cpp
+++ labs/Lab_2/ver-vs2015/main.cpp
@@ -35,8 +35,6 @@ float eyez = 0.0;
 
 glm::mat4 projection;	// projection matrix
 
-
-
 float cx;
 float cy;
 float cz;
@@ -55,9 +53,9 @@ void init() {
 	GLint vNormal;
 	int vs;
 	int fs;
-	GLfloat *vertices; // vertex coordinate data read and stored here
-	GLfloat *normals; // vertex normals data read and stored here
-	GLushort *indices; // vertex indices data read and stored here
+	GLfloat (*vertices)[3]; // vertex coordinate data read and stored here
+	GLfloat (*normals)[3]; // vertex normals data read and stored here
+	GLuint *indices; // vertex indices data read and stored here
 	std::vector<tinyobj::shape_t> shapes; // shapes data read in from the obj file
 	std::vector<tinyobj::material_t> materials; // materials data read in from the obj file
 	int nv; // total number of vertices
@@ -69,34 +67,38 @@ void init() {
 
 	/*  Load the obj file */
 
-	tinyobj::LoadObj(shapes, materials, "Vase.obj");
+	tinyobj::LoadObj(shapes, materials, "Vase.obj", NULL);
 
 	/*  Retrieve the vertex coordinate data */
 
 
-	nv = shapes.at(0).mesh.positions.size();
-	vertices = new GLfloat[nv];
+	nv = shapes.at(0).mesh.positions.size()/3;
+	vertices = new GLfloat[nv][3];
 
-	int count = 0, xyz = 0;
+	int count = 0, xyz = 0, npositions = nv*3;
 	float xh = -999999.0f, yh = -999999.0f, zh = -999999.0f, xl = 9999999.0f, yl = 9999999.0f, zl = 9999999.0f;
 	while (count < nv) {
-		*vertices = shapes.at(0).mesh.positions.at(count);
-		//std::cout << *vertices << "\n";
+		//*vertices = shapes.at(0).mesh.positions.at(count);
+		vertices[count][0] = shapes.at(0).mesh.positions.at(count * 3);
+		vertices[count][1] = shapes.at(0).mesh.positions.at(count * 3 + 1);
+		vertices[count][2] = shapes.at(0).mesh.positions.at(count * 3 + 2);
+		//vertices[count][3] = 1.0f;
+		//		std::cout << *vertices << "\n";
 
 		if (xyz == 0) {
 			xyz++;
-			xh = std::max(*vertices, xh);
-			xl = std::min(*vertices, xl);
+			xh = std::max(vertices[count][0], xh);
+			xl = std::min(vertices[count][0], xl);
 		}
 		else if (xyz == 1) {
 			xyz++;
-			yh = std::max(*vertices, yh);
-			yl = std::min(*vertices, yl);
+			yh = std::max(vertices[count][1], yh);
+			yl = std::min(vertices[count][1], yl);
 		}
 		else {
 			xyz = 0;
-			zh = std::max(*vertices, zh);
-			zl = std::min(*vertices, zl);
+			zh = std::max(vertices[count][2], zh);
+			zl = std::min(vertices[count][2], zl);
 		}
 		count++;
 	}
@@ -109,46 +111,56 @@ void init() {
 
 
 
-	cx = abs(xh + xl)/2;
-	cy = abs(yh + yl)/2;
-	cz = abs(zh + zl)/2;
+	cx = (xh + xl) / 2;
+	cy = (yh + yl) / 2;
+	cz = (zh + zl) / 2;
+
 	printf("center: %f %f %f\n", cx, cy, cz); // cx, cy and cz are the coordinates of the centre
 
 	/*  Retrieve the vertex normals */
-	nn = shapes.at(0).mesh.positions.size();
-	normals = new GLfloat[nn];
+	nn = shapes.at(0).mesh.positions.size()/3;
+	normals = new GLfloat[nn][3];
 	count = 0;
 	while (count < nn) {
-		*normals = shapes.at(0).mesh.normals.at(count);
+		//*normals = shapes.at(0).mesh.normals.at(count);
+		normals[count][0] = shapes.at(0).mesh.normals.at(count * 3);
+		normals[count][1] = shapes.at(0).mesh.normals.at(count * 3 + 1);
+		normals[count][2] = shapes.at(0).mesh.normals.at(count * 3 + 2);
 		count++;
 	}
 
 	/*  Retrieve the triangle indices */
 
 	ni = shapes.at(0).mesh.indices.size();
-	indices = new GLushort[ni];
-	
+	indices = new GLuint[ni];
+
 	count = 0;
-		while (count < ni) {
-		*indices = shapes.at(0).mesh.indices.at(count);
+	while (count < ni) {
+		indices[count] = shapes.at(0).mesh.indices.at(count);
 		count++;
 	}
+
+	std::cout << "ni " << ni << "\n";
+	std::cout << "nn " << nn << "\n";
+	std::cout << "nv " << nv << "\n";
+	triangles = ni/3;
+
 
 	/*
 	*  load the vertex coordinate data
 	*/
 	glGenBuffers(1, &vbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
-	glBufferData(GL_ARRAY_BUFFER, (nv + nn)*sizeof(GLfloat), NULL, GL_STATIC_DRAW);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, nv*sizeof(GLfloat), vertices);
-	glBufferSubData(GL_ARRAY_BUFFER, nv*sizeof(GLfloat), nn*sizeof(GLfloat), normals);
+	glBufferData(GL_ARRAY_BUFFER, nv*sizeof(GLfloat[3]) + nn*sizeof(GLfloat[3]), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, nv*sizeof(GLfloat[3]), vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, nv*sizeof(GLfloat[3]), nn*sizeof(GLfloat[3]), normals);
 
 	/*
 	*  load the vertex indexes
 	*/
 	glGenBuffers(1, &ibuffer);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, ni*sizeof(GLushort), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, ni*sizeof(GLuint), indices, GL_STATIC_DRAW);
 
 	/*
 	*  compile and build the shader program
@@ -167,7 +179,7 @@ void init() {
 	glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(vPosition);
 	vNormal = glGetAttribLocation(program, "vNormal");
-	glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*) sizeof(vertices));
+	glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*) (nv*sizeof(GLfloat[3])));
 	glEnableVertexAttribArray(vNormal);
 
 }
@@ -189,9 +201,7 @@ void changeSize(int w, int h) {
 	glViewport(0, 0, w, h);
 
 	projection = glm::perspective(45.0f, ratio, 1.0f, 800.0f);
-
 }
-
 /*
 *  This procedure is called each time the screen needs
 *  to be redisplayed
@@ -213,10 +223,8 @@ void displayFunc() {
 	normalLoc = glGetUniformLocation(program, "normalMat");
 	glUniformMatrix3fv(normalLoc, 1, 0, glm::value_ptr(normal));
 	glBindVertexArray(objVAO);
-	glDrawElements(GL_TRIANGLES, 3 * triangles, GL_UNSIGNED_SHORT, NULL);
+	glDrawElements(GL_TRIANGLES, 3 * triangles, GL_UNSIGNED_INT, NULL);
 	glutSwapBuffers();
-
-
 }
 
 /*
@@ -279,7 +287,13 @@ int main(int argc, char **argv) {
 		printf("Error starting GLEW: %s\n", glewGetErrorString(error));
 		exit(0);
 	}
+	eyex = 0.0;
+	eyey = 500.0;
+	eyez = 0.0;
 
+	theta = 1.5;
+	phi = 1.5;
+	r = 500.0;
 	glutDisplayFunc(displayFunc);
 	glutIdleFunc(idleFunc);
 	glutReshapeFunc(changeSize);
@@ -288,7 +302,7 @@ int main(int argc, char **argv) {
 	init();
 
 	glEnable(GL_DEPTH_TEST);
-	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClearColor(.5, .5, .5, 1);
 
 	glutMainLoop();
 
