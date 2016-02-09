fe9d7ef0815761ea16467796712a9bef9fe44377
Jasdeep Nijjar <jasdeepn4@hotmail.com>
2016-02-09 11:33:42 -0500

    adding assignment 1 files

---
+++ assignments/a1/Question One/main.cpp
@@ -0,0 +1,219 @@
+/************************************************
+ *
+ *
+ *
+ ************************************************/
+
+#include <Windows.h>
+#include <gl/glew.h>
+#include <gl/glut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "shaders.h"
+#include <stdio.h>
+
+GLuint program;			// shader programs
+GLuint triangleVAO;		// the data to be displayed
+float angle = 0.0;
+int window;
+
+glm::mat4 projection;	// projection matrix
+float eyex, eyey, eyez;	// eye position
+
+/*
+ *  The init procedure creates the OpenGL data structures
+ *  that contain the triangle geometry, compiles our
+ *  shader program and links the shader programs to
+ *  the data.
+ */
+
+void init() {
+	GLuint vbuffer;
+	GLuint ibuffer;
+	GLint vPosition;
+	GLint vNormal;
+	int vs;
+	int fs;
+
+	glGenVertexArrays(1, &triangleVAO);
+	glBindVertexArray(triangleVAO);
+
+	GLfloat vertices[3][2] = {	// coordinates of triangle vertices
+		{ -0.5, -0.5 },
+		{  0.0,  0.5},
+		{  0.5, -0.5}
+	};
+
+	GLfloat normals[3][3] = {
+		{0.0, 0.0, 1.0},
+		{0.0, 0.0, 1.0},
+		{0.0, 0.0, 1.0}
+	};
+
+	GLushort indexes[3] = { 0, 1, 2 };	// indexes of triangle vertices
+
+	/*
+	 *  load the vertex coordinate data
+	 */
+	glGenBuffers(1, &vbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices)+sizeof(normals), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(normals), normals);
+	
+	/*
+	 *  load the vertex indexes
+	 */
+	glGenBuffers(1, &ibuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+
+	/*
+	 *  compile and build the shader program
+	 */
+	vs = buildShader(GL_VERTEX_SHADER, "vertex_shader.vs");
+	fs = buildShader(GL_FRAGMENT_SHADER, "fragment_shader.fs");
+	program = buildProgram(vs,fs,0);
+
+	/*
+	 *  link the vertex coordinates to the vPosition
+	 *  variable in the vertex program
+	 */
+	glUseProgram(program);
+	vPosition = glGetAttribLocation(program,"vPosition");
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(vPosition);
+	vNormal = glGetAttribLocation(program, "vNormal");
+	glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*) sizeof(vertices));
+	glEnableVertexAttribArray(vNormal);
+
+}
+
+/*
+ *  Executed each time the window is resized,
+ *  usually once at the start of the program.
+ */
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	
+	if(h == 0)
+		h = 1;
+
+	float ratio = 1.0 * w / h;
+
+    glViewport(0, 0, w, h);
+
+	projection = glm::perspective(45.0f, ratio, 1.0f, 100.0f);
+	
+}
+
+/*
+ *  This procedure is called each time the screen needs
+ *  to be redisplayed
+ */
+void displayFunc() {
+	glm::mat4 model;
+	glm::mat4 view;
+	glm::mat4 modelViewPerspective;
+	int modelLoc;
+	int normalLoc;
+
+	model = glm::rotate(glm::mat4(1.0), angle, glm::vec3(0.0, 1.0, 0.0));
+
+	view = glm::lookAt(glm::vec3(eyex, eyey, eyez),
+					glm::vec3(0.0f, 0.0f, 0.0f),
+					glm::vec3(0.0f, 1.0f, 0.0f));
+
+	glm::mat3 normal = glm::transpose(glm::inverse(glm::mat3(view*model)));
+					
+	modelViewPerspective = projection * view * model;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(program);
+	modelLoc = glGetUniformLocation(program,"model");
+	glUniformMatrix4fv(modelLoc, 1, 0, glm::value_ptr(modelViewPerspective));
+	normalLoc = glGetUniformLocation(program,"normalMat");
+	glUniformMatrix3fv(normalLoc, 1, 0, glm::value_ptr(normal));
+
+	glBindVertexArray(triangleVAO);
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, NULL);
+
+	glutSwapBuffers();
+
+}
+
+/*
+ *  Update the value of angle on each update
+ */
+void idleFunc() {
+
+	glutSetWindow(window);
+	angle = angle + 0.1;
+	glutPostRedisplay();
+
+}
+
+/*
+ *  Called each time a key is pressed on
+ *  the keyboard.
+ */
+void keyboardFunc(unsigned char key, int x, int y) {
+
+	switch(key) {
+	case 'a':
+		eyey -= 0.1;
+		break;
+	case 'd':
+		eyey += 0.1;
+		break;
+	case 'w':
+		eyex += 0.1;
+		break;
+	case 's':
+		eyex -= 0.1;
+		break;
+	}
+	glutPostRedisplay();
+
+}
+
+int main(int argc, char **argv) {
+
+	/*
+	 *  initialize glut, set some parameters for
+	 *  the application and create the window
+	 */
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(320,320);
+	window = glutCreateWindow("Assignment One");
+
+	/*
+	 *  initialize glew
+	 */
+	GLenum error = glewInit();
+	if(error != GLEW_OK) {
+		printf("Error starting GLEW: %s\n",glewGetErrorString(error));
+		exit(0);
+	}
+
+	glutDisplayFunc(displayFunc);
+	glutIdleFunc(idleFunc);
+	glutReshapeFunc(changeSize);
+	glutKeyboardFunc(keyboardFunc);
+
+	eyex = 0.0;
+	eyey = 0.0;
+	eyez = 4.0;
+
+	init();
+
+	glClearColor(1.0,1.0,1.0,1.0);
+
+	glutMainLoop();
+
+}
